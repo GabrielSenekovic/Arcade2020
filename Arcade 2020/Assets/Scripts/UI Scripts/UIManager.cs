04232fb6f905184f7c533517ef9481e70779cba8
Gabriel Senekovic <Gabriel.Senekovic@gmail.com>
2020-04-14 00:30:47 +0200

    Pause screen exists
    But its a purple square

---
+++ Arcade 2020/Assets/Scripts/UI Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]CanvasGroup pauseScreen;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            OpenOrClose();
+        }
+    }
+
+    void OpenOrClose()
+    {
+        Debug.Log("Trying to turn on and off pause screen");
+        pauseScreen.alpha = pauseScreen.alpha > 0 ? 0 : 1;
+        pauseScreen.blocksRaycasts = pauseScreen.blocksRaycasts == true ? false : true;
+    }
+}
