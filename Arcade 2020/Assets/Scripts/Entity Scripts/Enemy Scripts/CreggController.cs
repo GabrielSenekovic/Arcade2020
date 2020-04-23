b02e055568ed702e58193fc5c548c008b14436ee
Casper G <caspergustavsson5@gmail.com>
2020-04-23 19:59:05 +0200

    start of cregg code and fixed scene a bit

---
+++ Arcade 2020/Assets/Scripts/Entity Scripts/Enemy Scripts/CreggController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreggController : Movement
+{
+    public enum MoveType {HORIZONTAL, VERTICAL };
+    private MoveType movetype;
+    void Start()
+    {
+        Speed = 7.0f;
+        if( movetype == MoveType.HORIZONTAL) { Dir = new Vector2(1,0); }
+        else { Dir = new Vector2(0,1);}
+    }
+
+    void Update()
+    {
+        
+
+        MoveObject();
+    }
+
+     private void OnCollisionEnter2D(Collision2D other) 
+    {
+        Dir *= -1;
+        //* if collide player hurt (attack) 
+    }
+}
