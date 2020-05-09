7c5de05f73c9b2ca34c88cf548418ae3a857fb00
Gabriel Senekovic <Gabriel.Senekovic@gmail.com>
2020-05-09 21:18:13 +0200

    Fixed minimap again

---
+++ Arcade 2020/Assets/Scripts/UI Scripts/Minimap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    List<Vector2> addedRooms = new List<Vector2>(){};
+    [SerializeField] GameObject roomIcon;
+
+    private void Awake() 
+    {
+        addedRooms.Add(new Vector2(0,0));
+    }
+    public void AddRoomToMap(Vector2 newLocation)
+    {
+        if(!addedRooms.Contains(newLocation))
+        {
+            addedRooms.Add(newLocation);
+            Instantiate(roomIcon, new Vector3(newLocation.x + 12, newLocation.y + 9.5f, roomIcon.transform.position.z), Quaternion.identity, transform);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
