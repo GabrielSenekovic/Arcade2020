6caaf2614882e2093807619667026267fbc92ee0
Gabriel Senekovic <Gabriel.Senekovic@gmail.com>
2020-06-03 22:00:21 +0200

    Can now navigate menu with arrow keys

---
+++ Arcade 2020/Assets/Scripts/UI Scripts/MenuNavigator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuNavigator : MonoBehaviour
+{
+    public Button[] buttons;
+    int currentIndex = 0;
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if(currentIndex == 0)
+            {
+                currentIndex = buttons.Length-1;
+            }
+            else
+            {
+                currentIndex--;
+            }
+            buttons[currentIndex].Select();
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if(currentIndex == buttons.Length -1)
+            {
+                currentIndex = 0;
+            }
+            else
+            {
+                currentIndex++;
+            }
+            buttons[currentIndex].Select();
+        }
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            ExecuteEvents.Execute(buttons[currentIndex].gameObject, new BaseEventData(EventSystem.current), ExecuteEvents.submitHandler);
+        }
+    }
+}
