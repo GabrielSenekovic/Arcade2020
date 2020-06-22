131de827a1ca72eeba7baa94748926d81440e912
Gabriel Senekovic <Gabriel.Senekovic@gmail.com>
2020-06-22 22:07:16 +0200

    Intro music now loops

---
+++ Arcade 2020/Assets/Scripts/Audio Scripts/PlayIntro.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayIntro : MonoBehaviour
+{
+    public AudioClip intro;
+    public AudioClip menuTheme;
+    AudioSource music;
+
+    private void Start() 
+    {
+        music = GetComponent<AudioSource>();
+        StartCoroutine(playMusic());
+    }
+    
+    public IEnumerator playMusic()
+    {
+        Debug.Log(music.clip.length);
+        music.clip = intro;
+        music.Play();
+        yield return new WaitForSeconds(music.clip.length);
+        music.Stop();
+        music.loop = true;
+        music.clip = menuTheme;
+        music.Play();
+    }
+}
