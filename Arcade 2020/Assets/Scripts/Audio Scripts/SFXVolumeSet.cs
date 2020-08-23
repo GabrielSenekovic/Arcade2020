70c9582e9c20f8407b2d495081316d1d1b24068b
Nicolas Hammar <nico.hammar@gmail.com>
2020-08-23 17:31:30 +0200

    Options volume sliders work for all sounds that are not in the audiomanager

---
+++ Arcade 2020/Assets/Scripts/Audio Scripts/SFXVolumeSet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SFXVolumeSet : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetLevel(float sliderValue)
+    {
+        audioMixer.SetFloat("SFXVol", Mathf.Log10(sliderValue) * 20);
+    }
+}
